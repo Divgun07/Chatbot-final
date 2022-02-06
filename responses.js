5d9a674e349883e22131d98ce92465a474e17f2e
Divgun07 <71441340+Divgun07@users.noreply.github.com>
2022-02-06 23:34:01 +0530

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,140 @@
+function getBotResponse(input) {
+    
+    // question 1
+    if (input == "what is ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "define ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "what do you understand by ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "what is the term ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "what do you know about ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "can you explain me about ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "please explain me something about ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "can you explain me something about ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "what do you mean by ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+    if (input == "what do you mean by the term ewaste"  ) {
+        return "E-waste is electronic products that are unwanted, not working, and nearing or at the end of their “useful life.” Computers, televisions, VCRs, stereos, copiers, and fax machines are everyday electronic products.";
+    } 
+
+    // question 4
+
+    if (input == "how to reduce ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "how to minimise ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "how can we reduce ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "how can we minimise ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "what is being done to reduce ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "what is being done to minimise ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "how are we promoting reduction of ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "what are different ways to minimise ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "how do we reduce ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "how do we minimise ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "what can we do to reduce ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+    if (input == "what can we do to minimise ewaste"  ) {
+        return "Be a good consumer. Reuse as often as possible. Educate yourself on what gets put into your electronics. ";
+    } 
+
+    // question 5
+    if (input == "how to reuse ewaste "  ) {
+        return "Regift to a friend or family member. Donate to a school, community center, nonprofit, Salvation Army, Goodwill, or charitable organization. Sell or giveaway using services like Gazelle or Freecycle";
+    } 
+    if (input == "how can we reuse ewaste"  ) {
+        return "Regift to a friend or family member. Donate to a school, community center, nonprofit, Salvation Army, Goodwill, or charitable organization. Sell or giveaway using services like Gazelle or Freecycle";
+    } 
+    if (input == "what is being done to reuse ewaste"  ) {
+        return "Regift to a friend or family member. Donate to a school, community center, nonprofit, Salvation Army, Goodwill, or charitable organization. Sell or giveaway using services like Gazelle or Freecycle";
+    } 
+    if (input == "how do we reuse ewaste"  ) {
+        return "Regift to a friend or family member. Donate to a school, community center, nonprofit, Salvation Army, Goodwill, or charitable organization. Sell or giveaway using services like Gazelle or Freecycle";
+    } 
+    if (input == "what can we do to reuse ewaste"  ) {
+        return "Regift to a friend or family member. Donate to a school, community center, nonprofit, Salvation Army, Goodwill, or charitable organization. Sell or giveaway using services like Gazelle or Freecycle";
+    } 
+    if (input == "what can we do to minimise ewaste"  ) {
+        return "Regift to a friend or family member. Donate to a school, community center, nonprofit, Salvation Army, Goodwill, or charitable organization. Sell or giveaway using services like Gazelle or Freecycle";
+    }   
+
+    // question 6
+    if (input == "how to recycle ewaste"  ) {
+        return "Collecting and Transporting. This is the first stage of recycling e-waste. Shredding and sorting. Dust Extraction. Magnetic separation. Purification of waste stream. Preparing recycled materials for sale";
+    } 
+    if (input == "how can we recycle ewaste"  ) {
+        return "Collecting and Transporting. This is the first stage of recycling e-waste. Shredding and sorting. Dust Extraction. Magnetic separation. Purification of waste stream. Preparing recycled materials for sale";
+    } 
+    if (input == "what is being done to recycle ewaste"  ) {
+        return "Collecting and Transporting. This is the first stage of recycling e-waste. Shredding and sorting. Dust Extraction. Magnetic separation. Purification of waste stream. Preparing recycled materials for sale";
+    } 
+    if (input == "how do we recycle ewaste"  ) {
+        return "Collecting and Transporting. This is the first stage of recycling e-waste. Shredding and sorting. Dust Extraction. Magnetic separation. Purification of waste stream. Preparing recycled materials for sale";
+    } 
+    if (input == "what can we do to recycle ewaste"  ) {
+        return "Collecting and Transporting. This is the first stage of recycling e-waste. Shredding and sorting. Dust Extraction. Magnetic separation. Purification of waste stream. Preparing recycled materials for sale";
+    } 
+    if (input == "what are different ways to recycle ewaste"  ) {
+        return "Collecting and Transporting. This is the first stage of recycling e-waste. Shredding and sorting. Dust Extraction. Magnetic separation. Purification of waste stream. Preparing recycled materials for sale";
+    } 
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } 
+    else if (input == "hi") {
+        return "Hello there!";
+    } else if (input == "bye") {
+        return "Bye! Talk to you later!";
+    } else if (input == "") {
+        return "Sorry could not understand this!";
+    }
+    else if (input == "thanks for your assistance") {
+        return "Bye! Talk to you later!";
+    }
+    else if (input == "thanks") {
+        return "Bye! Talk to you later!";
+    }
+    else if (input == "thankyou") {
+        return "Bye! Talk to you later!";
+    }
+    else if (input == "hey") {
+        return "Hello there!";
+    } 
+    else {
+        return "Try asking something else!";
+    }
+}
